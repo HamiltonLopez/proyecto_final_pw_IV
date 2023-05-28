37d166ffe9986ba719d8a060d47c93f5f6168f32
unknown <hamilt727@gmail.com>
2023-05-28 16:02:04 -0500

    estructura del apartado hombre

--- layout/templates/caballero/main-content.php
+++ layout/templates/caballero/main-content.php
@@ -1,2 +1,43 @@
+<div class="man container">
+    <div class="cards row row-cols-sm-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 " >
+    <div class="col d-flex justify-content-center">
+    <div class="card">
+      <img src="img/reloj_1.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Título de la tarjeta</h5>
+        <p class="card-text">Esta es una tarjeta más larga con texto de apoyo a continuación como introducción natural a contenido adicional. Este contenido es un poco más largo.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col d-flex justify-content-center">
+    <div class="card ">
+      <img src="img/reloj_2.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Título de la tarjeta</h5>
+        <p class="card-text">Esta es una tarjeta más larga con texto de apoyo a continuación como introducción natural a contenido adicional. Este contenido es un poco más largo.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col d-flex justify-content-center">
+    <div class="card">
+      <img src="img/reloj_3.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Título de la tarjeta</h5>
+        <p class="card-text">Esta es una tarjeta más larga con texto de apoyo a continuación como introducción natural a contenido adicional.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col d-flex justify-content-center">
+    <div class="card">
+      <img src="img/reloj_4.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Título de la tarjeta</h5>
+        <p class="card-text">Esta es una tarjeta más larga con texto de apoyo a continuación como introducción natural a contenido adicional. Este contenido es un poco más largo.</p>
+      </div>
+    </div>
+  </div>
+  
+    </div>
 
-    <h1 class="text-center">Aqui va el contenido de pagina de caballero</h1></div>
+
+</div>
