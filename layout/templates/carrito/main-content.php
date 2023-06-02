65cbb10b484fbc209a84cf9de826e7854981b330
Dany Villalba <villalba.daniela.0874@eam.edu.co>
2023-06-01 21:11:10 -0500

    Probando interfaz carrito

--- layout/templates/carrito/main-content.php
+++ layout/templates/carrito/main-content.php
@@ -1,2 +1,63 @@
+<?php
+  /*require_once ($BASE_ROOT_FOLDER_PATH.'includes/database.php');
+  require($BASE_ROOT_FOLDER_PATH.'classes/data/Carrito.php');
+  $carrito = new Carrito();
+  $registros = $carrito->getAll(); //obtenemos todos los registros de la tabla*/
+  echo $_SESSION['module_title'];
+?>
 
-    <h1 class="text-center">Aqui va el contenido de pagina de carrito</h1></div>
+<div class="container">
+    <div class="row align-items-center py-5" id="show-content">
+      <div class="col-12 text-center">
+        <h1>CARRITO DE COMPRAS</h1>
+      </div>
+      <div class="table-responsive">
+        <table class="table table-bordered align-middle" id="edit-table">
+          <thead class="table-light">
+            <th>#</th>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Tipo Documento</th>
+            <th># Documento</th>
+            <th>Dirección</th>
+            <th>Teléfono</th>
+            <th>Correo electrónico</th>
+            <th>Fecha de nacimiento</th>
+            <th>Acción</th>
+          </thead>
+          <tbody>
+            <?php
+              if(count($registros) < 1){
+            ?>
+              <tr>
+                <td colspan="7">No hay registros</td>
+              </tr>
+            <?php
+              } else {
+
+                foreach($registros as $index => $fila) {
+            ?>
+                <tr>
+                  <td><?php echo ($index + 1)?></td>
+                  <td><?php echo $fila['name']?></td>
+                  <td><?php echo $fila['lastname']?></td>
+                  <td><?php echo $fila['document_type_name']?></td>
+                  <td><?php echo $fila['document_number']?></td>
+                  <td><?php echo $fila['address']?></td>
+                  <td><?php echo $fila['phone']?></td>
+                  <td><?php echo $fila['email']?></td>
+                  <td><?php echo strlen($fila['birthday'])>=10?substr($fila['birthday'],0,10):''?></td>
+                  <td>
+                      <input type="button" value="Borrar" onClick="borrar_registro(<?php echo $fila['id']; ?>);">
+                      <input type="button" value="Editar" onClick="editar_registro(<?php echo $fila['id']; ?>);">
+                  </td>
+                </tr>
+            <?php
+                }
+              }
+            ?>
+          </tbody>
+        </table>
+      </div>    
+    </div>
+</div>
