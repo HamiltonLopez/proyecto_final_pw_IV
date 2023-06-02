18a87da0ff7b72411f187a050e1b5c95ff3c8d09
Dany Villalba <villalba.daniela.0874@eam.edu.co>
2023-06-02 15:35:56 -0500

    Creado el archivo actions.js de caballero

--- assets/js/caballero/actions.js
+++ assets/js/caballero/actions.js
@@ -1,5 +1,3 @@
 function añadir_producto(idReloj){
-    if(confirm("Está seguro de eliminar el registro?")){
-      location.replace(BASE_ROOT_URL_PATH+'controller/Carrito/addProduct.php?id='+idReloj);
-    }
+  location.replace(BASE_ROOT_URL_PATH+'controller/Carrito/addProduct.php?id='+idReloj);
 }
