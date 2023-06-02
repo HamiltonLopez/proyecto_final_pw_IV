beea4679686ebc4b61eb161ab4b3c1ffb61334a1
Dany Villalba <villalba.daniela.0874@eam.edu.co>
2023-06-02 13:32:10 -0500

    Corregido el main-content del carrito

--- layout/templates/carrito/main-content.php
+++ layout/templates/carrito/main-content.php
@@ -1,6 +1,6 @@
 <?php
-  /*require_once ($BASE_ROOT_FOLDER_PATH.'includes/database.php');
-  require($BASE_ROOT_FOLDER_PATH.'classes/data/Carrito.php');
+  /*require_once ($BASE_ROOT_FOLDER_PATH.'configs/database.php');
+  require($BASE_ROOT_FOLDER_PATH.'classes/Carrito.php');
   $carrito = new Carrito();
   $registros = $carrito->getAll(); //obtenemos todos los registros de la tabla*/
   //echo $_SESSION['module_title'];
@@ -35,17 +35,13 @@
             ?>
                 <tr>
                   <td><?php echo ($index + 1)?></td>
-                  <td><?php echo $fila['name']?></td>
-                  <td><?php echo $fila['lastname']?></td>
-                  <td><?php echo $fila['document_type_name']?></td>
-                  <td><?php echo $fila['document_number']?></td>
-                  <td><?php echo $fila['address']?></td>
-                  <td><?php echo $fila['phone']?></td>
-                  <td><?php echo $fila['email']?></td>
-                  <td><?php echo strlen($fila['birthday'])>=10?substr($fila['birthday'],0,10):''?></td>
+                  <td><?php echo $fila['nombreReloj']?></td>
+                  <td><?php echo $fila['nombreTipo']?></td>
+                  <td><?php echo $fila['modeloReloj']?></td>
+                  <td><?php echo $fila['cantidadRelojes']?></td>
                   <td>
-                      <input type="button" value="Borrar" onClick="borrar_registro(<?php echo $fila['id']; ?>);">
-                      <input type="button" value="Editar" onClick="editar_registro(<?php echo $fila['id']; ?>);">
+                      <input type="button" value="Borrar" onClick="borrar_registro(<?php echo $fila['idCarrito']; ?>);">
+                      <input type="button" value="Editar" onClick="editar_registro(<?php echo $fila['idCarrito']; ?>);">
                   </td>
                 </tr>
             <?php
